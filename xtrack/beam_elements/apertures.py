22ab00397f33115079815933c4eca73538d178f4
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-21 23:38:27 +0200

    Updated apertures

--- xtrack/beam_elements/apertures.py
+++ xtrack/beam_elements/apertures.py
@@ -1,15 +1,15 @@
 import xobjects as xo
 
-from ..dress_element import dress_element
+from ..dress_element import BeamElement
 from ..general import _pkg_root
 
-class LimitRectData(xo.Struct):
-    min_x = xo.Float64
-    max_x = xo.Float64
-    min_y = xo.Float64
-    max_y = xo.Float64
-LimitRectData.extra_sources = [
+class LimitRect(BeamElement):
+    _xofields={
+        'min_x': xo.Float64,
+        'max_x': xo.Float64,
+        'min_y': xo.Float64,
+        'max_y': xo.Float64,
+        }
+LimitRect.XoStruct.extra_sources = [
         _pkg_root.joinpath('beam_elements/apertures_src/limitrect.h')]
 
-class LimitRect(dress_element(LimitRectData)):
-    pass
